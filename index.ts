af696051cc2e0cf0fa40021b3ab24aabe6f37094
Sluip430 <s12122000@gmail.com>
2022-02-02 11:21:00 +0200

    123

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import config from './src/configurations/config';
 import { router } from './src/routers/router';
-
-require('./src/database/database');
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger/swagger.json";
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
+app.options('*', cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.use('/', router);
